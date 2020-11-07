15aa38c1f9587aa9c3c9e494b28b909dbcbbf6d1
Ugur Tigu <ugurtigu@gmail.com>
2020-11-07 12:24:06 +0100

    first working scrape

--- klickscraper/klickscraper/spiders/heftig.py
+++ klickscraper/klickscraper/spiders/heftig.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy import Request
+import json
 
 
 class HeftigSpider(scrapy.Spider):
@@ -6,5 +8,24 @@ class HeftigSpider(scrapy.Spider):
     allowed_domains = ['heftig.de']
     start_urls = ['http://heftig.de/']
 
+    
+    def start_requests(self):
+        for page in range(9):
+            yield Request(
+                url=f'https://www.heftig.de/_main_json/{page}.json',
+                 dont_filter=True
+            )
+
+    
     def parse(self, response):
-        pass
+        results = json.loads(response.body)
+        feed_list = results["feed"]
+        for feed_element in feed_list:
+            yield {'id': feed_element['id'],
+            'headline': feed_element['headline'],
+            'img': feed_element['img'],
+            'link': feed_element['link'],
+            'date': feed_element['date'],
+            'date_line': feed_element['date_line']
+            }
+            
